e6056a9be6029af7377a3b01e0fc1fb5ac32b528
Geovanna Melo <gsfmelo@gmail.com>
2026-08-25 15:22:52 -0300

    cv added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -66,7 +66,7 @@ const badges = [
           
           <div class="btn-row reveal reveal-delay-3">
             <RouterLink to="/projetos" class="btn primary">Ver projetos</RouterLink>
-            <a href="/cv-geovanna-melo.pdf" download="CV_GeovannaMelo_Tech.pdf" target="_blank" class="btn ghost">Baixar currículo</a>
+            <a href="/curriculo-geovannamelo.pdf" download="CV_GeovannaMelo_Dev.pdf" target="_blank" class="btn ghost">Baixar currículo</a>
             <a href="https://github.com/gsfmelo" target="_blank" class="btn ghost github-btn">
               <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/>
